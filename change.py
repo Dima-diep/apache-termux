ffd4938a71ef5cbde898c7215a22ae619bb92947
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-16 17:46:13 +0400

    change.py

--- change.py
+++ change.py
@@ -6,20 +6,11 @@ print("Find pair strings with \"DocumentRoot\" and change it")
 print("1.nano")
 print("2.micro")
 print("3.joe")
-print("4.vim")
-print("5.emacs")
-print("6.neovim")
 a = int(input())
 
 if a == 1:
     os.system("apt install nano && nano /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
 elif a == 2:
-    os.system("apt install micro && micro /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
+    os.system("apt install micro && clear && echo "Ctrl-S for save, Ctrl-Q for exit" && sleep 10s && micro /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
 elif a == 3:
-    os.system("apt install joe && joe /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
-elif a == 4:
-    os.system("apt install vim && vim /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
-elif a == 5:
-    os.system("apt install emacs && emacs /data/data/com.termux/fi    les/usr/etc/apache2/httpd.conf")
-elif a == 6:
-    os.system("apt install neovim && nvim /data/data/com.termux/fi    les/usr/etc/apache2/httpd.conf")
+    os.system("apt install joe && clear && echo "Ctrl-K-Q for exit" && sleep 10s && joe /data/data/com.termux/files/usr/etc/apache2/httpd.conf")
